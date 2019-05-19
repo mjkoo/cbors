98b0f8ed1fdfcbb0401f2e5fb4e97ea8bc0b9c11
Maxwell Koo <mjkoo90@gmail.com>
2019-05-19 04:05:33 -0600

    Fix coverage again

--- fuzz/fuzz_targets/fuzz_cbors.rs
+++ fuzz/fuzz_targets/fuzz_cbors.rs
@@ -1,7 +1,12 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate cbors;
+use libfuzzer_sys::fuzz_target;
+
+use cbors::cbors;
+use pyo3::prelude::*;
+use pyo3::{wrap_pymodule};
 
 fuzz_target!(|data: &[u8]| {
-
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let module = wrap_pymodule!(cbors)(py);
 });
