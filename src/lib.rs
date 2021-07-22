0c38477162393357358283c48bc3da77ba94b514
Maxwell Koo <mjkoo90@gmail.com>
2021-07-21 23:29:46 -0600

    Fix clippy command, fix audit

--- src/lib.rs
+++ src/lib.rs
@@ -5,38 +5,11 @@ use pyo3::prelude::*;
 use pyo3::types::{PyAny, PyByteArray, PyBytes, PyDict, PyList};
 use pyo3::{wrap_pyfunction, AsPyPointer, FromPyObject, PyObject, ToPyObject};
 use serde::{Deserialize, Serialize};
-use serde_cbor::{ObjectKey, Value};
-
-/// A simplified CBOR value containing only types useful for keys.
-#[derive(Debug, Clone, Eq, Ord, PartialOrd, PartialEq, Deserialize, Serialize)]
-struct CborObjectKey(ObjectKey);
+use serde_cbor::value::{Value};
 
 /// An enum over all possible CBOR types.
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 struct CborValue(Value);
-
-impl<'source> FromPyObject<'source> for CborObjectKey {
-    fn extract(ob: &'source PyAny) -> PyResult<Self> {
-        if ob.as_ptr() == unsafe { pyo3::ffi::Py_None() } {
-            Ok(CborObjectKey(ObjectKey::Null))
-        } else if let Ok(b) = ob.extract::<bool>() {
-            Ok(CborObjectKey(ObjectKey::Bool(b)))
-        } else if let Ok(i) = ob.extract::<i64>() {
-            Ok(CborObjectKey(ObjectKey::Integer(i)))
-        } else if let Ok(s) = ob.extract::<String>() {
-            Ok(CborObjectKey(ObjectKey::String(s)))
-        } else if let Ok(b) = ob.downcast::<PyByteArray>() {
-            Ok(CborObjectKey(ObjectKey::Bytes(b.to_vec())))
-        } else if let Ok(b) = ob.downcast::<PyBytes>() {
-            Ok(CborObjectKey(ObjectKey::Bytes(b.as_bytes().to_vec())))
-        } else {
-            Err(PyTypeError::new_err(format!(
-                "Value not convertable to cbor object key: {}",
-                ob.to_string()
-            )))
-        }
-    }
-}
 
 impl<'source> FromPyObject<'source> for CborValue {
     fn extract(ob: &'source PyAny) -> PyResult<Self> {
@@ -44,14 +17,12 @@ impl<'source> FromPyObject<'source> for CborValue {
             Ok(CborValue(Value::Null))
         } else if let Ok(b) = ob.extract::<bool>() {
             Ok(CborValue(Value::Bool(b)))
-        } else if let Ok(u) = ob.extract::<u64>() {
-            Ok(CborValue(Value::U64(u)))
-        } else if let Ok(i) = ob.extract::<i64>() {
-            Ok(CborValue(Value::I64(i)))
+        } else if let Ok(i) = ob.extract::<i128>() {
+            Ok(CborValue(Value::Integer(i)))
         } else if let Ok(f) = ob.extract::<f64>() {
-            Ok(CborValue(Value::F64(f)))
+            Ok(CborValue(Value::Float(f)))
         } else if let Ok(s) = ob.extract::<String>() {
-            Ok(CborValue(Value::String(s)))
+            Ok(CborValue(Value::Text(s)))
         } else if let Ok(b) = ob.downcast::<PyByteArray>() {
             Ok(CborValue(Value::Bytes(b.to_vec())))
         } else if let Ok(b) = ob.downcast::<PyBytes>() {
@@ -66,10 +37,10 @@ impl<'source> FromPyObject<'source> for CborValue {
                     .collect::<PyResult<Vec<_>>>()?,
             )))
         } else if let Ok(d) = ob.downcast::<PyDict>() {
-            Ok(CborValue(Value::Object(
+            Ok(CborValue(Value::Map(
                 d.into_iter()
                     .map(|(k, v)| {
-                        let ck: CborObjectKey = k.extract()?;
+                        let ck: CborValue = k.extract()?;
                         let cv: CborValue = v.extract()?;
                         Ok((ck.0, cv.0))
                     })
@@ -84,38 +55,26 @@ impl<'source> FromPyObject<'source> for CborValue {
     }
 }
 
-impl ToPyObject for CborObjectKey {
-    fn to_object(&self, py: Python) -> PyObject {
-        match &self.0 {
-            ObjectKey::Null => py.None(),
-            ObjectKey::Bool(b) => b.to_object(py),
-            ObjectKey::Integer(i) => i.to_object(py),
-            ObjectKey::String(s) => s.to_object(py),
-            ObjectKey::Bytes(v) => PyBytes::new(py, &v).into(),
-        }
-    }
-}
-
 impl ToPyObject for CborValue {
     fn to_object(&self, py: Python) -> PyObject {
         match &self.0 {
             Value::Null => py.None(),
             Value::Bool(b) => b.to_object(py),
-            Value::U64(u) => u.to_object(py),
-            Value::I64(i) => i.to_object(py),
-            Value::F64(f) => f.to_object(py),
-            Value::String(s) => s.to_object(py),
+            Value::Integer(i) => i.to_object(py),
+            Value::Float(f) => f.to_object(py),
+            Value::Text(s) => s.to_object(py),
             Value::Bytes(v) => PyBytes::new(py, &v).into(),
             Value::Array(a) => a
                 .iter()
                 .map(|x| CborValue(x.clone()))
                 .collect::<Vec<_>>()
                 .to_object(py),
-            Value::Object(d) => d
+            Value::Map(d) => d
                 .iter()
-                .map(|(k, v)| (CborObjectKey(k.clone()), CborValue(v.clone())))
+                .map(|(k, v)| (CborValue(k.clone()), CborValue(v.clone())))
                 .collect::<BTreeMap<_, _>>()
                 .to_object(py),
+            _ => py.None(),
         }
     }
 }
